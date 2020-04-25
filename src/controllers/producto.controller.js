78a30f8d9cf0dd3efd5c5adf60a812588fd11e20
kevav27 <38163635+kevav27@users.noreply.github.com>
2020-04-25 04:16:26 -0600

    crud productos

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -3,98 +3,69 @@ const productoCtrl = {};
 // Models
 const Producto = require("../models/producto.model");
 
+productoCtrl.renderForm = async (req, res) => {
+  const productos = await Producto.find()
+  res.render("productos", { productos })
+}
+
 productoCtrl.crearNuevoProducto = async (req, res) => {
-  const code = Math.floor((Math.random()*10000000));
-  const nuevoProducto = new Producto({
-    codigo_producto: "PRO-"+code,
-    descripcion: req.body.descripcion,
-    puntoReorden: req.body.puntoReorden,
-    unidadMedida: req.body.unidadMedida,
-    codigo_materiaPrima: req.body.codigo_materiaPrima,
-  });
-  await nuevoProducto.save()
-    .then(nuevoProducto => {
-      res.json(nuevoProducto);
-    }).catch(err => {
-      res.status(500).send({
-        message: err.message || "Error al crear nuevo camión"
-      });
+  try {
+    const code = Math.floor((Math.random() * 10000000));
+    req.body.codigo_producto = "CL-" + code;
+    req.body.codigo_materiaPrima = "MP-"+code;
+    const nuevoProducto = new Producto({
+      codigo_producto: req.body.codigo_producto,
+      descripcion: req.body.descripcion,
+      nombreCorto: req.body.nombreCorto,
+      puntoReorden: req.body.puntoReorden,
+      unidadDeMedida: req.body.unidadDeMedida,
+      codigo_materiaPrima: req.body.codigo_materiaPrima,
     });
+    await nuevoProducto.save()
+    res.redirect('/producto/add')
+  } catch(err){
+    res.render('pantallaError', { err })
+  }
 };
 
 productoCtrl.listarProducto = async (req, res) => {
-  await Producto.find()
-    .then(producto => {
-      res.send(producto);
-    }).catch(err => {
-      res.status(500).send({
-        message: err.message || "Error al recuperar bodegas de la base de datos"
-      });
-    });
+  const producto = await Producto.find()
 };
 
 
 productoCtrl.obtenerProducto = async (req, res) => {
- await Producto.find({codigo_producto: req.params.codigo_producto})
-    .then(producto => {
-      if (!producto) {
-        return res.status(404).send({
-          message: "El camion con el código: " + req.params.codigo_bodega + " no existe"
-        });
-      }
-      res.send(producto);
-    }).catch(err => {
-      if (err.kind === 'ObjectId') {
-        return res.status(404).send({
-          message: "No se encontró ningún camión con el código: " + req.params.codigo_bodega
-        });
-      }
-      return res.status(500).send({
-        message: "Error al recuperar camión código: " + req.params.codigo_bodega
-      });
-    });
+  try {
+    const producto =  await Producto.find({ codigo_producto: req.params.codigo_producto })
+    res.render('productos', {producto})
+    }
+    catch (err) {
+      res.render('pantallaError', { err })
+    }
+}
+
+productoCtrl.renderEdit = async (req, res) => {
+  const producto = await Producto.find({ codigo_producto: req.params.codigo_producto })
+  res.render('productos-edit', {producto})
 }
 
 productoCtrl.actualizarProducto = async (req, res) => {
-  await Producto.findOneAndUpdate({codigo_producto: req.params.codigo_producto}, { $set: req.body }, { new: true})
-    .then(producto => {
-      if (!producto) {
-        return res.status(404).send({
-          message: "No existe el camión con el código: " +req.params.codigo_bodega
-        });
-      }
-      res.send(producto)
-    }).catch(err => {
-      if (err.kind === 'ObjectId') {
-        return res.status(404).send({
-          message: "No se encontró un camión con el código: " + req.params.codigo_bodega
-        });
-      }
-      return res.status(500).send({
-        message: "Error al intentar actualizar el camión con el código: " + req.params.codigo_bodega
-      });
-    });
+  try{
+    await Producto.findOneAndUpdate({ codigo_producto: req.params.codigo_producto }, 
+      { $set: req.body },
+      { new: true })
+      res.redirect('/producto/add')
+    }catch(err){
+      res.render('pantallaError', { err })
+    }
 };
 
 productoCtrl.eliminarProducto = async (req, res) => {
-  await Producto.deleteOne({codigo_producto: req.params.codigo_producto})
-    .then(producto => {
-      if (!producto) {
-        return res.status(404).send({
-          message: "No existe el camión con el código: " + req.params.codigo_bodega
-        });
-      }
-      res.send({ "message": "Se eliminó el camión" })
-    }).catch(err => {
-      if (err.kind === 'ObjectId') {
-        return res.status(404).send({
-          message: "No se encontró un camión con el código: " + req.params.codigo_bodega
-        });
-      }
-      return res.status(500).send({
-        message: `Error al intentar eliminar el camión con el código: ${req.params.codigo_bodega}`
-      });
-    });
+  try{
+    await Producto.deleteOne({ codigo_producto: req.params.codigo_producto })
+    res.redirect('/producto/add')
+    }catch(err){
+      res.render('pantallaError', { err })
+    }
 };
 
 module.exports = productoCtrl;
